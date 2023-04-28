75ef88cfd9b4ae0f2a00c712b9850166b81e00af
YJ-928 <yashbjoshi.99@gmail.com>
2023-04-28 09:53:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-'''Number Guessing Game Objectives:-
-Include an ASCII art logo.
-Allow the player to submit a guess for a number between 1 and 100.
-Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-If they got the answer correct, show the actual answer to the player.
-Track the number of turns remaining.
-If they run out of turns, provide feedback to the player.
-Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).'''
+# Number Guessing Game Objectives:-
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player.
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
 import os
 import random
@@ -17,7 +17,7 @@ from time import sleep
 
 def guess_the_number():
     print('\n', logo, "\nWelcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    difficulty = input("\nChoose a difficulty. Type 'easy' or 'hard': ")
 
     choosen_number = random.choice(numbers)
 
@@ -29,24 +29,24 @@ def guess_the_number():
 
     while number_of_guesses != 0:
         guess = int(input(
-            f"You have {number_of_guesses} attempts remaining to guess the number.\nMake a guess: "))
+            f"\nYou have {number_of_guesses} attempts remaining to guess the number.\nMake a guess: "))
         if guess == int(choosen_number):
-            print(f"You got it! The answer was {choosen_number}.")
+            print(f"\nYou got it! The answer was {choosen_number}.")
             break
-        elif guess > int(choosen_number):
-            print("Too High.\nGuess again.")
-        elif guess < int(choosen_number):
-            print("Too Low.\nGuess again.")
         number_of_guesses -= 1
         if number_of_guesses == 0:
             print(
                 f"You've run out of guesses, you lose.\nThe answer was {choosen_number}.")
             break
+        elif guess > int(choosen_number):
+            print("\nToo High.\nGuess again.")
+        elif guess < int(choosen_number):
+            print("\nToo Low.\nGuess again.")
 
 
 # for playing first time by directly running code.
 guess_the_number()
 
-if input("Do you want to play again?, Type 'y' for Yes, 'n' for No: ").lower() == 'y':
+if input("\nDo you want to play again?, Type 'y' for Yes, 'n' for No: ").lower() == 'y':
     os.system('cls')  # clearing the screen of console
     guess_the_number()
